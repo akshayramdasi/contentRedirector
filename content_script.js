be279b9a9b014e9761f8a8b64f5b1c7b15ad9f57
akshayramdasi <akshayramdasi@yahoo.in>
2017-12-02 11:54:04 +0530

    a new comment added for code to be inspected

--- content_script.js
+++ content_script.js
@@ -5,7 +5,7 @@ $('#buttonForBlocking').click(function(event) {
 	
 });
 
-
+//folllowing code needs to be inspected
 chrome.runtime.sendMessage("ping");
 
 chrome.runtime.onMessage.addListener(function(response,sender,sendResponse) {
